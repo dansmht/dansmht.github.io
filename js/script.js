506572425ff40df40a3e35269ce9bcc82b95495a
advden4ik <dan12gm@mail.ru>
2020-01-27 03:26:04 +0300

    Added Map with toggle button and added target='_blank' to links

--- js/script.js
+++ js/script.js
@@ -15,4 +15,32 @@
       })
     })
   })
+
+
+  const coords = [52.44685, 30.993]
+  const map = L.map('mapid').setView(coords, 16)
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGFuc21odCIsImEiOiJjazV2ZHliOTUwcGN6M2xvMDBsem1xNmk2In0.qe4wKA3hbwwt3X3NEclELA',
+  }).addTo(map)
+
+  const icon = L.icon({
+    iconUrl: './images/location-pin.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
+    popupAnchor: [0, -40],
+  })
+  L.marker(coords, {icon}).addTo(map).bindPopup('<b>Kirova 122a</b>').openPopup()
+
+  const myMap = document.getElementById('mapid')
+  myMap.classList.add('hide')
+  const toggleMapBtn = document.getElementById('show-map')
+  toggleMapBtn.addEventListener('change', () => {
+    toggleMapBtn.checked
+      ? myMap.classList.remove('hide')
+      : myMap.classList.add('hide')
+  })
 })()
